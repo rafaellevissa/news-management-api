e40fff06d82a9064c136f98d9591491340833ca6
rafaellevissa <rafaellevissa@gmail.com>
2025-04-22 08:19:53 -0300

    Add auth and user crud

---
+++ backend/users/views.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets, permissions
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'destroy']:
+            return [permissions.IsAdminUser()]
+        elif self.action in ['retrieve', 'update', 'partial_update']:
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+
